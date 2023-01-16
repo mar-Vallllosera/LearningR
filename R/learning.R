6b3b8983d49f57e6386f4052c77c73fc870cb123
Mar Vallllosera <marv@chalmers.se>
2023-01-16 15:58:56 +0100

    Starting learning R document

---
+++ R/learning.R
@@ -0,0 +1,23 @@
+10
+
+# R basics ----------------------------------------------------------------
+weight_kilos <- 100
+weight_kilos <- 10
+weight_kilos
+
+colnames(airquality)
+
+str(airquality)
+summary(airquality)
+
+2 + 2
+2 + 2
+2 + 2
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
+
+r3::setup_git_config()
+r3::check_setup()
+r3::check_git_config()
